82f1ccf0dbcf490f7568a5df1e6ba25a4f2cd359
HeatherKates <hkates@ufl.edu>
2024-08-29 21:54:43 -0400

    added combined ppt

--- scripts/Run_analysis.R
+++ scripts/Run_analysis.R
@@ -10,6 +10,7 @@ analyze_dependent_variable("SNAP25.low.threshold.coverage")
 analyze_dependent_variable("SNAP25.high.threshold.coverage")
 analyze_dependent_variable("TH.Coverage")
 
+#Excel
 library(openxlsx)
 
 # Define the directory containing the CSV files
@@ -29,3 +30,40 @@ for (file in files) {
 }
 # Save the workbook
 saveWorkbook(wb, file = "results/results_tables/combined_results.xlsx", overwrite = TRUE)
+
+#Powerpoint
+# Define the main directory containing the subdirectories with PNG files
+main_dir <- "results/plots/"
+
+# List all subdirectories in the main directory
+subdirs <- list.dirs(main_dir, recursive = FALSE)
+
+# Initialize a new PowerPoint object
+ppt <- read_pptx()
+
+# Loop through each subdirectory
+for (subdir in subdirs) {
+  # Get the cleaned dependent variable name from the subdir path
+  cleaned_dependent_variable <- basename(subdir)
+  
+  # Add a title slide for the set of plots from this subdirectory
+  ppt <- add_slide(ppt, layout = "Title and Content", master = "Office Theme") %>%
+    ph_with(value = fpar(ftext(paste("Effect of predictor variables on", cleaned_dependent_variable),
+                               fp_text(bold = TRUE, font.size = 24))), 
+            location = ph_location_type(type = "title"))
+  
+  # List all PNG files in the current subdirectory
+  png_files <- list.files(subdir, pattern = "\\.png$", full.names = TRUE)
+  
+  # Loop through each PNG file and add it as a slide
+  for (png_file in png_files) {
+    ppt <- add_slide(ppt, layout = "Blank", master = "Office Theme") %>%
+      ph_with(value = external_img(png_file), location = ph_location_fullsize())
+  }
+}
+
+# Define the output PowerPoint file path in the main directory
+output_pptx <- paste0(main_dir, "combined_plots.pptx")
+
+# Save the PowerPoint presentation
+print(ppt, target = output_pptx)
--- scripts/model_selection_by_var.R
+++ scripts/model_selection_by_var.R
@@ -178,33 +178,6 @@ analyze_dependent_variable <- function(dependent_variable) {
       plot_list <- list()
     }
   }
-  library(officer)
-  library(magrittr)
-  
-  # Define the directory containing the PNG files
-  png_dir <- paste0("results/plots/",cleaned_dependent_variable,"/")
-  
-  # List all PNG files in the directory
-  png_files <-list.files(png_dir, pattern = "\\.png$", full.names = TRUE)
-  
-  ## Create a new PowerPoint object
-  ppt <- read_pptx()
-  
-  # Add a title slide with the title text
-  ppt <- add_slide(ppt, layout = "Title and Content", master = "Office Theme") %>%
-    ph_with(value = fpar(ftext(paste("Effect of predictor variables on",dependent_variable),fp_text(bold = TRUE, font.size = 24))), location = ph_location_type(type = "title"))
-  
-  # Loop through each PNG file and add it as a slide
-  for (png_file in png_files) {
-    ppt <- add_slide(ppt, layout = "Blank", master = "Office Theme") %>%
-      ph_with(value = external_img(png_file), location = ph_location_fullsize())
-  }
-  
-  # Define the output PowerPoint file path
-  output_pptx <- paste0(png_dir,cleaned_dependent_variable,"_plots.pptx")
-  
-  # Save the PowerPoint presentation
-  print(ppt, target = output_pptx)
   }
 
 
